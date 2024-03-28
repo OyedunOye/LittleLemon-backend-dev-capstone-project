461ee998bc70b64c603fb235d030ebd510729573
OyedunOye <oyesinaoyedun@yahoo.com>
2024-03-29 00:06:14 +0100

    added view classes related to both app models

--- LittleLemon/restaurant/views.py
+++ LittleLemon/restaurant/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+# from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from .models import Booking, Menu
+from .serializers import MenuSerializer, BookingSerializer
+# from django.contrib.auth.models import User
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html', {})
+
+# POST and GET view for menu using generic view
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+# GET, PUT & DELETE view for each menu record
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+# 
+class BookingViewSet(viewsets.ModelViewSet):
+    authentication_classes = [IsAuthenticated]
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
